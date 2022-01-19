fb73c993c01dde1e4edb95ad59c77ecbd3f580ca
Dutta-SD <duttasandip11100@gmail.com>
2022-01-20 00:27:20 +0530

    Added Custom Input and Live Prediction

--- app.py
+++ app.py
@@ -6,11 +6,18 @@ import gradio
 if __name__ == "__main__":
     interface = gradio.Interface(
         get_predictions,
-        inputs="text",
+        inputs=gradio.inputs.Textbox(
+            lines=2,
+            placeholder="Enter The Text",
+            default="",
+            label="Text to Predict",
+        ),
         outputs=["text", "text"],
         title="Aggression and Misogyny Predictor",
         theme="dark-huggingface",
+        live=True,
     )
     interface.launch(
         share=False,
+        debug=True,
     )
